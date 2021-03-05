fb55/htmlparser2

improved DomUtils
what I really wanted to do is now in comments (it failed multiple times)

lib/DomUtils.js
@@ -1,78 +1,65 @@
-module.exports = {
-    testElement: function(options, element) {
-        if (!element) return false;
+var ElementType = require("./ElementType.js");
 
-        var type = element.type;
+function getTest(checkVal) {
+    return function(value) {
+        return value === checkVal;
+    };
+}
 
-        for (var key in options) {
-            if (key === "tag_name") {
-                if (type !== "tag" && type !== "script" && type !== "style")
-                    return false;
-                if (!options.tag_name(element.name)) return false;
-            } else if (key === "tag_type") {
-                if (!options.tag_type(type)) return false;
-            } else if (key === "tag_contains") {
-                if (
-                    type !== "text" &&
-                    type !== "comment" &&
-                    type !== "directive"
-                )
-                    return false;
-                if (!options.tag_contains(element.data)) return false;
-            } else if (!element.attribs || !options[key](element.attribs[key]))
+function testElement(options, element) {
+    if (!element) return false;
+
+    var type = element.type;
+
+    for (var key in options) {
+        if (key === "tag_name") {
+            if (
+                type !== ElementType.Tag &&
+                type !== ElementType.Script &&
+                type !== ElementType.Style
+            )
                 return false;
-        }
+            if (!options.tag_name(element.name)) return false;
+        } else if (key === "tag_type") {
+            if (!options.tag_type(type)) return false;
+        } else if (key === "tag_contains") {
+            if (
+                type !== ElementType.Text &&
+                type !== ElementType.Comment &&
+                type !== ElementType.Directive
+            )
+                return false;
+            if (!options.tag_contains(element.data)) return false;
+        } else if (!element.attribs || !options[key](element.attribs[key]))
+            return false;
+    }
 
-        return true;
-    },
+    return true;
+}
+
+module.exports = {
+    testElement: testElement,
 
     getElements: function(options, currentElement, recurse, limit) {
-        if (!currentElement) return [];
+        recurse = recurse === undefined || recurse === null || recurse;
+        if (isNaN(limit)) limit = -1;
 
-        recurse = recurse === undefined || recurse === null || !!recurse;
-
-        var parsed_limit = parseInt(limit, 10);
-        limit = isNaN(parsed_limit) ? -1 : parsed_limit;
-
-        var found = [];
-        var elementList;
-
-        function getTest(checkVal) {
-            return function(value) {
-                return value === checkVal;
-            };
-        }
         for (var key in options) {
-            if (typeof options[key] !== "function") {
+            if (typeof options[key] !== "function")
                 options[key] = getTest(options[key]);
-            }
         }
-
-        if (this.testElement(options, currentElement)) {
-            found.push(currentElement);
-        }
-
-        if (limit >= 0 && found.length >= limit) return found;
-
-        if (recurse && currentElement.children)
-            elementList = currentElement.children;
-        else if (Array.isArray(currentElement)) elementList = currentElement;
-        else return found;
-
-        for (var i = 0; i < elementList.length; i++) {
-            found = found.concat(
-                this.getElements(options, elementList[i], recurse, limit)
-            );
-
-            if (limit >= 0 && found.length >= limit) break;
-        }
-
-        return found;
+        return this.testAttr(
+            testElement.bind(null, options),
+            currentElement,
+            recurse,
+            limit
+        );
     },
 
     getElementById: function(id, currentElement, recurse) {
         var result = this.getElements({ id: id }, currentElement, recurse, 1);
         return result.length ? result[0] : null;
+        //function(elem){return elem.attribs && elem.attribs.id === id;}
     },
 
     getElementsByTagName: function(name, currentElement, recurse, limit) {
@@ -82,14 +69,45 @@ module.exports = {
             recurse,
             limit
         );
+        /*function(elem){
+			var type = elem.type;
+			if(type !== ElementType.Tag && type !== ElementType.Script && type !== ElementType.Style) return false;
+			return elem.name === name;
+		};*/
     },
 
     getElementsByTagType: function(type, currentElement, recurse, limit) {
-        return this.getElements(
-            { tag_type: type },
+        return this.testAttr(
+            function(elem) {
+                return elem.type === type;
+            },
             currentElement,
             recurse,
             limit
         );
+        //function(elem){return elem.type === type;}
+    },
+
+    testAttr: function(test, element, recurse, limit) {
+        var found = [],
+            elementList;
+        if (!element) return found;
+        if (test(element)) found.push(element);
+
+        if (recurse && element.children) elementList = element.children;
+        else if (Array.isArray(element)) elementList = element;
+        else return found;
+
+        for (
+            var i = 0, j = elementList.length;
+            i < j && (limit < 0 || found.length < limit);
+            i++
+        ) {
+            found = found.concat(
+                this.testAttr(test, elementList[i], recurse, limit)
+            );
+        }
+
+        return found;
     }
 };

fasterize/node-htmlparser

lib/DomUtils.js
@@ -1,74 +1,76 @@
+var ElementType = require("./ElementType.js");
+
+function getTest (checkVal) {
+	return function (value) { return value === checkVal; };
+}
+
+function testElement(options, element) {
+	if (!element) return false;
+    
+    var type = element.type;
+
+    for (var key in options) {
+    	if (key === "tag_name") {
+    		if (type !== ElementType.Tag && type !== ElementType.Script && type !== ElementType.Style) return false;
+    		if (!options.tag_name(element.name)) return false;
+    	} else if (key === "tag_type") {
+    		if (!options.tag_type(type)) return false;
+    	} else if (key === "tag_contains") {
+    		if (type !== ElementType.Text && type !== ElementType.Comment && type !== ElementType.Directive) return false;
+    		if (!options.tag_contains(element.data)) return false;
+    	} else if (!element.attribs || !options[key](element.attribs[key]))
+    		return false;
+    }
+
+    return true;
+}
+
 module.exports = {
-	testElement: function(options, element) {
-		if (!element) return false;
-		
-		var type = element.type;
-
-		for (var key in options) {
-			if (key === "tag_name") {
-				if (type !== "tag" && type !== "script" && type !== "style") return false;
-				if (!options.tag_name(element.name)) return false;
-			} else if (key === "tag_type") {
-				if (!options.tag_type(type)) return false;
-			} else if (key === "tag_contains") {
-				if (type !== "text" && type !== "comment" && type !== "directive") return false;
-				if (!options.tag_contains(element.data)) return false;
-			} else if (!element.attribs || !options[key](element.attribs[key]))
-				return false;
-		}
+	testElement: testElement, 
 	
-		return true;
-	}
+	getElements: function(options, currentElement, recurse, limit){
+		recurse = recurse === undefined || recurse === null || recurse;
+		if(isNaN(limit)) limit = -1;
 
-	, getElements: function(options, currentElement, recurse, limit) {
-		if (!currentElement) return [];
-
-		recurse = (recurse === undefined || recurse === null) || !!recurse;
-
-		var parsed_limit = parseInt(limit, 10);
-		limit = isNaN(parsed_limit) ? -1 : parsed_limit;
-
-		var found = [];
-		var elementList;
-
-		function getTest (checkVal) {
-			return function (value) { return value === checkVal; };
-		}
-		for (var key in options) {
-			if (typeof options[key] !== "function") {
+		for(var key in options){
+			if (typeof options[key] !== "function")
 				options[key] = getTest(options[key]);
-			}
 		}
-
-		if (this.testElement(options, currentElement)) {
-			found.push(currentElement);
-		}
-
-		if (limit >= 0 && found.length >= limit) return found;
-
-		if(recurse && currentElement.children) elementList = currentElement.children;
-		else if(Array.isArray(currentElement)) elementList = currentElement;
-		else return found;
-
-		for (var i = 0; i < elementList.length; i++) {
-			found = found.concat(this.getElements(options, elementList[i], recurse, limit));
-
-			if (limit >= 0 && found.length >= limit) break;
-		}
-
-		return found;
+		return this.testAttr(testElement.bind(null, options), currentElement, recurse, limit);
 	}
 	
 	, getElementById: function(id, currentElement, recurse) {
 		var result = this.getElements({ id: id }, currentElement, recurse, 1);
 		return result.length ? result[0] : null;
+		//function(elem){return elem.attribs && elem.attribs.id === id;}
 	}
 	
 	, getElementsByTagName: function(name, currentElement, recurse, limit) {
 		return this.getElements({ tag_name: name }, currentElement, recurse, limit);
+		/*function(elem){
+			var type = elem.type;
+			if(type !== ElementType.Tag && type !== ElementType.Script && type !== ElementType.Style) return false;
+			return elem.name === name;
+		};*/
 	}
 	
-	, getElementsByTagType: function(type, currentElement, recurse, limit) {
-		return this.getElements({ tag_type: type }, currentElement, recurse, limit);
+	, getElementsByTagType: function(type, currentElement, recurse, limit){
+		return this.testAttr(function(elem){return elem.type === type;}, currentElement, recurse, limit);
+		//function(elem){return elem.type === type;}
 	}
-};
+	
+	, testAttr: function(test, element, recurse, limit){
+		var found = [], elementList;
+		if(!element) return found;
+		if(test(element)) found.push(element);
+		
+		if(recurse && element.children) elementList = element.children;
+		else if(Array.isArray(element)) elementList = element;
+		else return found;
+		
+		for(var i = 0, j = elementList.length; i < j && (limit < 0 || found.length < limit); i++){
+			found = found.concat(this.testAttr(test, elementList[i], recurse, limit));
+		}
+		
+		return found;
+	}};