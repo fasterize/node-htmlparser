fb55/htmlparser2

Cleaned up DomUtils
* Attempted to perform some optimization
* Fixed some style inconsistencies
* Added test for DomUtils HTML methods

lib/DomUtils.js
@@ -1,12 +1,7 @@
-var ElementType = require("./ElementType.js");
+var ElementType = require("./ElementType.js"),
+    arrayPush = Array.prototype.push,
+    DomUtils = module.exports;
 
-function getTest(checkVal) {
-    return function(value) {
-        return value === checkVal;
-    };
-}
-
-var arrayPush = Array.prototype.push;
 function filterArray(test, arr, recurse, limit) {
     var result = [],
         childs;
@@ -17,31 +12,36 @@ function filterArray(test, arr, recurse, limit) {
             if (--limit <= 0) break;
         }
 
-        if (recurse && (childs = arr[i].children)) {
+        childs = arr[i].children;
+        if (recurse && childs) {
             childs = filterArray(test, childs, recurse, limit);
             arrayPush.apply(result, childs);
             limit -= childs.length;
             if (limit <= 0) break;
         }
     }
+
     return result;
 }
 
 function filter(test, element, recurse, limit) {
     if (recurse !== false) recurse = true;
-    if (isNaN(limit)) limit = 1 / 0;
-    if (!Array.isArray(element)) {
-        element = [element];
-    }
+    if (isNaN(limit)) limit = Infinity;
+    if (!Array.isArray(element)) element = [element];
+
     return filterArray(test, element, recurse, limit);
 }
 
-module.exports = {
-    testElement: function testElement(options, element) {
-        var type = element.type;
+DomUtils.testElement = function(options, element) {
+    var type = element.type,
+        keys = Object.keys(options),
+        len = keys.length;
 
-        for (var key in options) {
-            if (key === "tag_name") {
+    for (var i = 0; i < len; i++) {
+        var key = keys[i];
+
+        switch (key) {
+            case "tag_name":
                 if (
                     type !== ElementType.Tag &&
                     type !== ElementType.Script &&
@@ -49,9 +49,11 @@ module.exports = {
                 )
                     return false;
                 if (!options.tag_name(element.name)) return false;
-            } else if (key === "tag_type") {
+                break;
+            case "tag_type":
                 if (!options.tag_type(type)) return false;
-            } else if (key === "tag_contains") {
+                break;
+            case "tag_contains":
                 if (
                     type !== ElementType.Text &&
                     type !== ElementType.Comment &&
@@ -59,70 +61,68 @@ module.exports = {
                 )
                     return false;
                 if (!options.tag_contains(element.data)) return false;
-            } else if (!element.attribs || !options[key](element.attribs[key]))
-                return false;
+                break;
+            default:
+                if (!element.attribs || !options[key](element.attribs[key]))
+                    return false;
+                break;
         }
+    }
 
-        return true;
-    },
+    return true;
+};
 
-    getElements: function(options, element, recurse, limit) {
-        for (var key in options) {
-            if (typeof options[key] !== "function") {
-                options[key] = getTest(options[key]);
-            }
+DomUtils.getElements = function(options, element, recurse, limit) {
+    var keys = Object.keys(options),
+        len = keys.length;
+
+    for (var i = 0; i < len; i++) {
+        var key = keys[i];
+
+        if (typeof options[key] !== "function") {
+            var checker = options[key];
+            options[key] = function(val) {
+                return val === checker;
+            };
         }
+    }
 
-        return filter(
-            this.testElement.bind(null, options),
+    return filter(
+        this.testElement.bind(null, options),
+        element,
+        recurse,
+        limit
+    );
+};
+
+DomUtils.getElementById = function(id, element, recurse) {
+    var result;
+
+    if (typeof id === "function") {
+        result = filter(
+            function(elem) {
+                return elem.attribs && id(elem.attribs);
+            },
             element,
             recurse,
-            limit
+            1
         );
-    },
+    } else {
+        result = filter(
+            function(elem) {
+                return elem.attribs && elem.attribs.id === id;
+            },
+            element,
+            recurse,
+            1
+        );
+    }
 
-    getElementById: function(id, element, recurse) {
-        var result;
-        if (typeof id === "function") {
-            result = filter(
-                function(elem) {
-                    return elem.attribs && id(elem.attribs);
-                },
-                element,
-                recurse,
-                1
-            );
-        } else {
-            result = filter(
-                function(elem) {
-                    return elem.attribs && elem.attribs.id === id;
-                },
-                element,
-                recurse,
-                1
-            );
-        }
-        return result.length ? result[0] : null;
-    },
+    return result.length ? result[0] : null;
+};
 
-    getElementsByTagName: function(name, element, recurse, limit) {
-        if (typeof name === "function")
-            return filter(
-                function(elem) {
-                    var type = elem.type;
-                    if (
-                        type !== ElementType.Tag &&
-                        type !== ElementType.Script &&
-                        type !== ElementType.Style
-                    )
-                        return false;
-                    return name(elem.name);
-                },
-                element,
-                recurse,
-                limit
-            );
-
+DomUtils.getElementsByTagName = function(name, element, recurse, limit) {
+    if (typeof name === "function")
         return filter(
             function(elem) {
                 var type = elem.type;
@@ -132,70 +132,98 @@ module.exports = {
                     type !== ElementType.Style
                 )
                     return false;
-                return elem.name === name;
+                return name(elem.name);
             },
             element,
             recurse,
             limit
         );
-    },
 
-    getElementsByTagType: function(type, element, recurse, limit) {
-        if (typeof type === "function") {
-            return filter(
-                function(elem) {
-                    return type(elem.type);
-                },
-                element,
-                recurse,
-                limit
-            );
-        } else
-            return filter(
-                function(elem) {
-                    return elem.type === type;
-                },
-                element,
-                recurse,
-                limit
-            );
-    },
+    return filter(
+        function(elem) {
+            var type = elem.type;
+            if (
+                type !== ElementType.Tag &&
+                type !== ElementType.Script &&
+                type !== ElementType.Style
+            )
+                return false;
+            return elem.name === name;
+        },
+        element,
+        recurse,
+        limit
+    );
+};
 
-    getInnerHTML: function(elem) {
-        if (!elem.children) return "";
+DomUtils.getElementsByTagType = function(type, element, recurse, limit) {
+    if (typeof type === "function")
+        return filter(
+            function(elem) {
+                return type(elem.type);
+            },
+            element,
+            recurse,
+            limit
+        );
+    else
+        return filter(
+            function(elem) {
+                return elem.type === type;
+            },
+            element,
+            recurse,
+            limit
+        );
+};
 
-        var childs = elem.children,
-            childNum = childs.length,
-            ret = "";
+DomUtils.getInnerHTML = function(elem) {
+    if (!elem.children) return "";
 
-        for (var i = 0; i < childNum; i++) {
-            ret += this.getOuterHTML(childs[i]);
-        }
+    var childs = elem.children,
+        childNum = childs.length,
+        ret = "";
 
-        return ret;
-    },
-
-    getOuterHTML: function(elem) {
-        var type = elem.type;
-
-        if (type === ElementType.Text) return elem.data;
-        if (type === ElementType.Comment) return "<!--" + elem.data + "-->";
-
-        var ret = "<" + elem.name;
-
-        var value;
-        for (var name in elem.attribs) {
-            value = elem.attribs[name];
-            ret += " " + name + "=";
-
-            if (/^[^\s"\'\`\=\<\>]+$/.test(value)) ret += value;
-            else if (value.indeOf('"') !== -1) ret += "'" + value + "'";
-            else ret += '"' + value + '"';
-        }
-
-        if (type === ElementType.Directive) return ret + ">";
-        if (type === ElementType.Tag && !elem.children) return ret + " />";
-
-        return ">" + ret + this.getInnerHTML(elem) + "</" + elem.name + ">";
+    for (var i = 0; i < childNum; i++) {
+        ret += this.getOuterHTML(childs[i]);
     }
+
+    return ret;
+};
+
+DomUtils.getOuterHTML = function(elem) {
+    var type = elem.type,
+        name = elem.name;
+
+    if (type === ElementType.Text) return elem.data;
+    if (type === ElementType.Comment) return "<!--" + elem.data + "-->";
+
+    var attrStr = "";
+    if (elem.attribs) {
+        var attrs = Object.keys(elem.attribs),
+            len = attrs.length;
+
+        for (var i = 0; i < len; i++) {
+            var attr = attrs[i],
+                val = elem.attribs[attr];
+
+            attrStr += " " + attr + '="' + val + '"';
+
+            /* Is this required? Method forgets quotes
+            if(/^[^\s"\'\`\=\<\>]+$/.test(val))
+                attrStr += val;
+            else if(val.indeOf("\"") !== -1)
+                attrStr += "'" + val + "'";
+            else
+                attrStr += "\"" + val + "\"";
+            */
+        }
+    }
+
+    var ret = "<" + name + attrStr + ">";
+
+    if (type === ElementType.Directive) return ret;
+
+    ret += this.getInnerHTML(elem) + "</" + name + ">";
+    return ret;
 };

fasterize/node-htmlparser

lib/DomUtils.js
@@ -1,139 +1,163 @@
-var ElementType = require("./ElementType.js");
+var ElementType = require("./ElementType.js"),
+    arrayPush = Array.prototype.push,
+    DomUtils = module.exports;
 
-function getTest (checkVal) {
-	return function (value) { return value === checkVal; };
-}
-
-var arrayPush = Array.prototype.push;
 function filterArray(test, arr, recurse, limit){
-	var result = [], childs;
-	
-	for(var i = 0, j = arr.length; i < j; i++){
-		if(test(arr[i])){
-			result.push(arr[i]);
-			if(--limit <= 0) break;
-		}
-		
-		if(recurse && (childs = arr[i].children)){
-			childs = filterArray(test, childs, recurse, limit);
-			arrayPush.apply(result, childs);
-			limit -= childs.length;
-			if(limit <= 0) break;
-		}
-	}
-	return result;
+    var result = [], childs;
+
+    for(var i = 0, j = arr.length; i < j; i++){
+        if(test(arr[i])){
+            result.push(arr[i]);
+            if(--limit <= 0) break;
+        }
+
+        childs = arr[i].children;
+        if(recurse && childs){
+            childs = filterArray(test, childs, recurse, limit);
+            arrayPush.apply(result, childs);
+            limit -= childs.length;
+            if(limit <= 0) break;
+        }
+    }
+
+    return result;
 }
 
 function filter(test, element, recurse, limit){
-	if(recurse !== false) recurse = true;
-	if(isNaN(limit)) limit = 1/0;
-	if(!Array.isArray(element)){
-		element = [element];
-	}
-	return filterArray(test, element, recurse, limit);
+    if(recurse !== false) recurse = true;
+    if(isNaN(limit)) limit = Infinity;
+    if(!Array.isArray(element)) element = [element];
+
+    return filterArray(test, element, recurse, limit);
 }
 
-module.exports = {
-	testElement: function testElement(options, element) {
-		 var type = element.type;
-	
-		for(var key in options){
-			if(key === "tag_name"){
-				if(type !== ElementType.Tag && type !== ElementType.Script && type !== ElementType.Style) return false;
-				if(!options.tag_name(element.name)) return false;
-			} else if(key === "tag_type") {
-				if(!options.tag_type(type)) return false;
-			} else if(key === "tag_contains") {
-				if(type !== ElementType.Text && type !== ElementType.Comment && type !== ElementType.Directive) return false;
-				if(!options.tag_contains(element.data)) return false;
-			} else if(!element.attribs || !options[key](element.attribs[key]))
-				return false;
-		}
-	
-		 return true;
-	}, 
-	
-	getElements: function(options, element, recurse, limit){
-		for(var key in options){
-			if(typeof options[key] !== "function"){
-				options[key] = getTest(options[key]);
-			}
-		}
-		
-		return filter(this.testElement.bind(null, options), element, recurse, limit);
-	},
+DomUtils.testElement = function(options, element){
+    var type = element.type,
+        keys = Object.keys(options),
+        len = keys.length;
 
-	getElementById: function(id, element, recurse) {
-		var result;
-		if(typeof id === "function"){
-			result = filter(function(elem){
-				return elem.attribs && id(elem.attribs);
-			}, element, recurse, 1);
-		}
-		else{
-			result = filter(function(elem){
-				return elem.attribs && elem.attribs.id === id;
-			}, element, recurse, 1);
-		}
-		return result.length ? result[0] : null;
-	},
+    for(var i = 0; i < len; i++){
+        var key = keys[i];
 
-	getElementsByTagName: function(name, element, recurse, limit){
-		if(typeof name === "function") return filter(function(elem){
-			var type = elem.type;
-			if(type !== ElementType.Tag && type !== ElementType.Script && type !== ElementType.Style) return false;
-			return name(elem.name);
-		}, element, recurse, limit);
-		
-		return filter(function(elem){
-			var type = elem.type;
-			if(type !== ElementType.Tag && type !== ElementType.Script && type !== ElementType.Style) return false;
-			return elem.name === name;
-		}, element, recurse, limit);
-	},
+        switch(key){
+            case "tag_name":
+                if(type !== ElementType.Tag && type !== ElementType.Script && type !== ElementType.Style) return false;
+                if(!options.tag_name(element.name)) return false;
+                break;
+            case "tag_type":
+                if(!options.tag_type(type)) return false;
+                break;
+            case "tag_contains":
+                if(type !== ElementType.Text && type !== ElementType.Comment && type !== ElementType.Directive) return false;
+                if(!options.tag_contains(element.data)) return false;
+                break;
+            default:
+                if(!element.attribs || !options[key](element.attribs[key])) return false;
+                break;
+        }
+    }
 
-	getElementsByTagType: function(type, element, recurse, limit){
-		if(typeof type === "function"){
-			return filter(function(elem){return type(elem.type);}, element, recurse, limit);
-		}
-		else return filter(function(elem){return elem.type === type;}, element, recurse, limit);
-	},
-	
-	getInnerHTML: function(elem){
-		if(!elem.children) return "";
-		
-		var childs = elem.children,
-			childNum = childs.length,
-			ret = "";
-		
-		for(var i = 0; i < childNum; i++){
-			ret += this.getOuterHTML(childs[i]);
-		}
-		
-		return ret;
-	},
-	
-	getOuterHTML: function(elem){
-		var type = elem.type;
+    return true;
+}
 
-		if(type === ElementType.Text) return elem.data;
-		if(type === ElementType.Comment) return "<!--" + elem.data + "-->";
-		
-		var ret = "<" + elem.name;
-		
-		var value;
-		for(var name in elem.attribs){
-			value = elem.attribs[name];
-			ret += " " + name + "=";
-			
-			if(/^[^\s"\'\`\=\<\>]+$/.test(value)) ret += value;
-			else if(value.indeOf("\"") !== -1) ret += "'" + value + "'";
-			else ret += "\"" + value + "\"";
-		}
-		
-		if(type === ElementType.Directive) return ret + ">";
-		if(type === ElementType.Tag && !elem.children) return ret + " />";
-		
-		return ">" + ret + this.getInnerHTML(elem) + "</" + elem.name + ">";
-	}
-};
+DomUtils.getElements = function(options, element, recurse, limit){
+    var keys = Object.keys(options),
+        len = keys.length;
+
+    for(var i = 0; i < len; i++){
+        var key = keys[i];
+
+        if(typeof options[key] !== "function"){
+            var checker = options[key];
+            options[key] = function(val){ return val === checker };
+        }
+    }
+
+    return filter(this.testElement.bind(null, options), element, recurse, limit);
+}
+
+DomUtils.getElementById = function(id, element, recurse){
+    var result;
+
+    if(typeof id === "function"){
+        result = filter(function(elem){ return elem.attribs && id(elem.attribs) }, element, recurse, 1);
+    }else{
+        result = filter(function(elem){ return elem.attribs && elem.attribs.id === id }, element, recurse, 1);
+    }
+
+    return result.length ? result[0] : null;
+}
+
+DomUtils.getElementsByTagName = function(name, element, recurse, limit){
+    if(typeof name === "function") return filter(function(elem){
+        var type = elem.type;
+        if(type !== ElementType.Tag && type !== ElementType.Script && type !== ElementType.Style)
+            return false;
+        return name(elem.name);
+    }, element, recurse, limit);
+
+    return filter(function(elem){
+        var type = elem.type;
+        if(type !== ElementType.Tag && type !== ElementType.Script && type !== ElementType.Style)
+            return false;
+        return elem.name === name;
+    }, element, recurse, limit);
+}
+
+DomUtils.getElementsByTagType = function(type, element, recurse, limit){
+    if(typeof type === "function")
+        return filter(function(elem){ return type(elem.type) }, element, recurse, limit);
+    else
+        return filter(function(elem){ return elem.type === type }, element, recurse, limit);
+}
+
+DomUtils.getInnerHTML = function(elem){
+    if(!elem.children) return "";
+
+    var childs = elem.children,
+        childNum = childs.length,
+        ret = "";
+
+    for(var i = 0; i < childNum; i++){
+        ret += this.getOuterHTML(childs[i]);
+    }
+
+    return ret;
+}
+
+DomUtils.getOuterHTML = function(elem){
+    var type = elem.type,
+        name = elem.name;
+
+    if(type === ElementType.Text) return elem.data;
+    if(type === ElementType.Comment) return "<!--" + elem.data + "-->";
+
+    var attrStr = "";
+    if(elem.attribs){
+        var attrs = Object.keys(elem.attribs),
+            len = attrs.length;
+
+        for(var i = 0; i < len; i++){
+            var attr = attrs[i],
+                val = elem.attribs[attr];
+
+            attrStr += " " + attr + "=\"" + val + "\"";
+
+            /* Is this required? Method forgets quotes
+            if(/^[^\s"\'\`\=\<\>]+$/.test(val))
+                attrStr += val;
+            else if(val.indeOf("\"") !== -1)
+                attrStr += "'" + val + "'";
+            else
+                attrStr += "\"" + val + "\"";
+            */
+        }
+    }
+
+    var ret = "<" + name + attrStr + ">";
+
+    if(type === ElementType.Directive) return ret;
+
+    ret += this.getInnerHTML(elem) + "</" + name + ">"
+    return ret;
+}