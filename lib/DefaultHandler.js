fb55/htmlparser2

Splited writeTag in both handlers to smaller functions + added a test for events

lib/DefaultHandler.js
@@ -4,22 +4,25 @@ function DefaultHandler(callback, options) {
     this.dom = [];
     this._done = false;
     this._tagStack = [];
-    this._options = options ? options : {};
-    if (this._options.ignoreWhitespace === undefined)
-        this._options.ignoreWhitespace = false; //Keep whitespace-only text nodes
-    if (this._options.verbose === undefined) this._options.verbose = true; //Keep data property for tags and raw property for all
-    if (this._options.enforceEmptyTags === undefined)
-        this._options.enforceEmptyTags = true; //Don't allow children for HTML tags defined as empty in spec
-    if (typeof callback === "function") this._callback = callback;
+    if (options) {
+        this._options = options;
+        if (typeof this._options.verbose === "undefined")
+            this._options.verbose = true;
+        if (typeof this._options.enforceEmptyTags === "undefined")
+            this._options.enforceEmptyTags = true;
+    }
+    this._callback = callback;
 }
 
-DefaultHandler.prototype._lastTag = function() {
-    var stack = this._tagStack;
-    return stack.length ? stack[stack.length - 1] : null;
+//default options
+DefaultHandler.prototype._options = {
+    ignoreWhitespace: false, //Keep whitespace-only text nodes
+    verbose: true, //Keep data property for tags and raw property for all
+    enforceEmptyTags: true //Don't allow children for HTML tags defined as empty in spec
 };
 
 //HTML Tags that shouldn't contain child nodes
-var _emptyTags = {
+var emptyTags = {
     area: true,
     base: true,
     basefont: true,
@@ -49,96 +52,61 @@ DefaultHandler.prototype.done = function() {
     this._done = true;
     this.handleCallback(null);
 };
-DefaultHandler.prototype.writeText = function(element) {
-    if (this._options.ignoreWhitespace) if (element.data.trim() === "") return;
-    this.handleElement(element);
-};
 
 //Methods//
 DefaultHandler.prototype.error = DefaultHandler.prototype.handleCallback = function(
     error
 ) {
-    if (typeof this._callback !== "function")
-        if (error) throw error;
-        else return;
-    this._callback(error, this.dom);
+    if (typeof this._callback === "function") this._callback(error, this.dom);
+    else if (error) throw error;
 };
 
-DefaultHandler.prototype.isEmptyTag = function(element) {
-    var name = element.name.toLowerCase();
-    if (name.charAt(0) === "/") {
-        name = name.substring(1);
-    }
-    return this._options.enforceEmptyTags && _emptyTags[name];
+DefaultHandler.prototype._isEmptyTag = function(name) {
+    return this._options.enforceEmptyTags && emptyTags[name];
 };
 
-DefaultHandler.prototype.writeTag = DefaultHandler.prototype.writeDirective = DefaultHandler.prototype.writeComment = DefaultHandler.prototype.handleElement = function(
-    element
-) {
-    if (this._done)
-        this.handleCallback(
-            new Error(
-                "Writing to the handler after done() called is not allowed without a reset()"
-            )
-        );
-    var couldBeContainer =
-        element.type === ElementType.Tag ||
-        element.type === ElementType.Script ||
-        element.type === ElementType.Style;
-    if (!this._options.verbose) {
-        //element.raw = null; //FIXME: Not clean
-        //FIXME: Serious performance problem using delete
-        delete element.raw;
-        if (couldBeContainer) delete element.data;
-    }
-    var lastTag = this._lastTag();
-    if (!lastTag) {
-        //There are no parent elements
-        //If the element can be a container, add it to the tag stack and the top level list
-        if (couldBeContainer) {
-            if (element.name.charAt(0) !== "/") {
-                //Ignore closing tags that obviously don't have an opening tag
-                this.dom.push(element);
-                if (!this.isEmptyTag(element)) {
-                    //Don't add tags to the tag stack that can't have children
-                    this._tagStack.push(element);
-                }
-            }
-        } //Otherwise just add to the top level list
-        else this.dom.push(element);
-    } else {
+DefaultHandler.prototype._closeTag = function(name) {
+    //Ignore closing tags that obviously don't have an opening tag
+    if (!this._tagStack || this._isEmptyTag(name)) return;
+
+    var pos = this._tagStack.length - 1;
+    while (pos !== -1 && this._tagStack[pos--].name !== name) {}
+    if (++pos !== 0 || this._tagStack[0].name === name)
+        while (pos < this._tagStack.length) this._tagStack.pop();
+};
+
+DefaultHandler.prototype._addDomElement = function(element) {
+    if (!this._options.verbose) delete element.raw;
+
+    var lastTag = this._tagStack[this._tagStack.length - 1];
+    if (!lastTag) this.dom.push(element);
+    else {
         //There are parent elements
-        //If the element can be a container, add it as a child of the element
-        //on top of the tag stack and then add it to the tag stack
-        if (couldBeContainer) {
-            if (element.name.charAt(0) === "/") {
-                //This is a closing tag, scan the tagStack to find the matching opening tag
-                //and pop the stack up to the opening tag's parent
-                var baseName = element.name.substring(1);
-                if (!this.isEmptyTag(element)) {
-                    var pos = this._tagStack.length - 1;
-                    while (
-                        pos !== -1 &&
-                        this._tagStack[pos--].name !== baseName
-                    ) {}
-                    if (pos !== -1 || this._tagStack[0].name === baseName)
-                        while (pos < this._tagStack.length - 1)
-                            this._tagStack.pop();
-                }
-            } else {
-                //This is not a closing tag
-                if (!lastTag.children) lastTag.children = [];
-                lastTag.children.push(element);
-                if (!this.isEmptyTag(element))
-                    //Don't add tags to the tag stack that can't have children
-                    this._tagStack.push(element);
-            }
-        } else {
-            //This is not a container element
-            if (!lastTag.children) lastTag.children = [];
-            lastTag.children.push(element);
-        }
+        if (!lastTag.children) lastTag.children = [element];
+        else lastTag.children.push(element);
     }
+};
+
+DefaultHandler.prototype._openTag = function(element) {
+    if (!this._options.verbose) delete element.data;
+
+    this._addDomElement(element);
+
+    //Don't add tags to the tag stack that can't have children
+    if (!this._isEmptyTag(element.name)) this._tagStack.push(element);
+};
+
+DefaultHandler.prototype.writeText = function(element) {
+    if (this._options.ignoreWhitespace && element.data.trim() === "") return;
+    this._addDomElement(element);
+};
+
+DefaultHandler.prototype.writeDirective = DefaultHandler.prototype.writeComment =
+    DefaultHandler.prototype._addDomElement;
+
+DefaultHandler.prototype.writeTag = function(element) {
+    if (element.name.charAt(0) === "/") this._closeTag(element.name.substr(1));
+    else this._openTag(element);
 };
 
 module.exports = DefaultHandler;

tests/00-runtests.js
@@ -10,6 +10,7 @@ var testCount = 0;
 var failedCount = 0;
 var totalTime = 0;
 var name = __filename.split("/").slice(-1)[0];
+var handler;
 for (var i = 1; i < testFiles.length; i++) {
     if (testFiles[i] === name) continue;
     testCount++;
@@ -20,16 +21,26 @@ for (var i = 1; i < testFiles.length; i++) {
     };
     console.log(testFiles[i]);
     var start = Date.now();
-    var handler =
-        test.type === "rss"
-            ? new htmlparser.RssHandler(handlerCallback, test.options.handler)
-            : new htmlparser.DefaultHandler(
-                  handlerCallback,
-                  test.options.handler
-              );
+    if (test.type === "rss") {
+        handler = new htmlparser.RssHandler(
+            handlerCallback,
+            test.options.handler
+        );
+    } else if (test.type === "event") {
+        handler = new htmlparser.EventedHandler(test.options.handler);
+    } else {
+        handler = new htmlparser.DefaultHandler(
+            handlerCallback,
+            test.options.handler
+        );
+    }
     var parser = new htmlparser.Parser(handler, test.options.parser);
     parser.parseComplete(test.html);
     var resultComplete = handler.dom;
+    if (test.type === "event") {
+        resultComplete = test.result;
+        test.result = [];
+    }
     var chunkPos = 0;
     parser.reset();
     while (chunkPos < test.html.length) {
@@ -38,6 +49,9 @@ for (var i = 1; i < testFiles.length; i++) {
     }
     parser.done();
     var resultChunk = handler.dom;
+    if (test.type === "event") {
+        resultChunk = test.result;
+    }
     var testResult =
         sys.inspect(resultComplete, false, null) ===
             sys.inspect(test.expected, false, null) &&

fasterize/node-htmlparser

lib/DefaultHandler.js
@@ -1,42 +1,28 @@
 var ElementType = require("./ElementType.js");
 
-function DefaultHandler (callback, options) {
+function DefaultHandler(callback, options){
 	this.dom = [];
 	this._done = false;
 	this._tagStack = [];
-	this._options = options ? options : { };
-	if (this._options.ignoreWhitespace === undefined)
-		this._options.ignoreWhitespace = false; //Keep whitespace-only text nodes
-	if (this._options.verbose === undefined)
-		this._options.verbose = true; //Keep data property for tags and raw property for all
-	if (this._options.enforceEmptyTags === undefined)
-		this._options.enforceEmptyTags = true; //Don't allow children for HTML tags defined as empty in spec
-	if ((typeof callback) === "function")
-		this._callback = callback;
+	if(options){
+		this._options = options;
+		if(typeof this._options.verbose === "undefined")
+			this._options.verbose = true;
+		if (typeof this._options.enforceEmptyTags === "undefined")
+			this._options.enforceEmptyTags = true;
+	}
+	this._callback = callback;
 }
 
-DefaultHandler.prototype._lastTag = function() {
-	var stack = this._tagStack;
-	return(stack.length ? stack[stack.length - 1] : null);
+//default options
+DefaultHandler.prototype._options = {
+	ignoreWhitespace: false,	//Keep whitespace-only text nodes
+    verbose: true,				//Keep data property for tags and raw property for all
+    enforceEmptyTags: true		//Don't allow children for HTML tags defined as empty in spec
 };
 
 //HTML Tags that shouldn't contain child nodes
-var _emptyTags = {
-	area: true
-	, base: true
-	, basefont: true
-	, br: true
-	, col: true
-	, frame: true
-	, hr: true
-	, img: true
-	, input: true
-	, isindex: true
-	, link: true
-	, meta: true
-	, param: true
-	, embed: true
-};
+var emptyTags={area:true,base:true,basefont:true,br:true,col:true,frame:true,hr:true,img:true,input:true,isindex:true,link:true,meta:true,param:true,embed:true};
 
 //**Public**//
 //Methods//
@@ -51,88 +37,60 @@ DefaultHandler.prototype.done = function() {
 	this._done = true;
 	this.handleCallback(null);
 };
-DefaultHandler.prototype.writeText = function(element) {
-	if(this._options.ignoreWhitespace)
-		if(element.data.trim() === "")
-			return;
-	this.handleElement(element);
-};
 
 //Methods//
 DefaultHandler.prototype.error =
-DefaultHandler.prototype.handleCallback = function(error) {
-		if ((typeof this._callback) !== "function")
-			if (error)
-				throw error;
-			else
-				return;
-		this._callback(error, this.dom);
+DefaultHandler.prototype.handleCallback = function(error){
+		if(typeof this._callback === "function")
+			this._callback(error, this.dom);
+		else if(error) throw error;
 };
 
-DefaultHandler.prototype.isEmptyTag = function(element) {
-	var name = element.name.toLowerCase();
-	if (name.charAt(0) === '/') {
-		name = name.substring(1);
-	}
-	return this._options.enforceEmptyTags && _emptyTags[name];
+DefaultHandler.prototype._isEmptyTag = function(name) {
+	return this._options.enforceEmptyTags && emptyTags[name];
 };
 
-DefaultHandler.prototype.writeTag = DefaultHandler.prototype.writeDirective = DefaultHandler.prototype.writeComment =
-DefaultHandler.prototype.handleElement = function(element) {
-	if (this._done)
-		this.handleCallback(new Error("Writing to the handler after done() called is not allowed without a reset()"));
-	var couldBeContainer = element.type === ElementType.Tag || element.type === ElementType.Script || element.type === ElementType.Style;
-	if (!this._options.verbose) {
-		//element.raw = null; //FIXME: Not clean
-		//FIXME: Serious performance problem using delete
-		delete element.raw;
-		if(couldBeContainer)
-			delete element.data;
+DefaultHandler.prototype._closeTag = function(name){
+	//Ignore closing tags that obviously don't have an opening tag
+	if(!this._tagStack || this._isEmptyTag(name)) return;
+	
+	var pos = this._tagStack.length - 1;
+	while (pos !== -1 && this._tagStack[pos--].name !== name) { }
+	if ( ++pos !== 0 || this._tagStack[0].name === name)
+	    while(pos < this._tagStack.length)
+	    	this._tagStack.pop();
+};
+
+DefaultHandler.prototype._addDomElement = function(element){
+	if(!this._options.verbose) delete element.raw;
+	
+	var lastTag = this._tagStack[this._tagStack.length-1];
+	if(!lastTag) this.dom.push(element);
+	else{ //There are parent elements
+		if(!lastTag.children) lastTag.children = [element];
+		else lastTag.children.push(element);
 	}
-	var lastTag = this._lastTag();
-	if (!lastTag) { //There are no parent elements
-		//If the element can be a container, add it to the tag stack and the top level list
-		if(couldBeContainer){
-			if (element.name.charAt(0) !== "/") { //Ignore closing tags that obviously don't have an opening tag
-				this.dom.push(element);
-				if (!this.isEmptyTag(element)) { //Don't add tags to the tag stack that can't have children
-					this._tagStack.push(element);
-				}
-			}
-		}
-		else //Otherwise just add to the top level list
-			this.dom.push(element);
-	}
-	else { //There are parent elements
-		//If the element can be a container, add it as a child of the element
-		//on top of the tag stack and then add it to the tag stack
-		if(couldBeContainer){
-			if (element.name.charAt(0) === "/") {
-				//This is a closing tag, scan the tagStack to find the matching opening tag
-				//and pop the stack up to the opening tag's parent
-				var baseName = element.name.substring(1);
-				if (!this.isEmptyTag(element)) {
-					var pos = this._tagStack.length - 1;
-					while (pos !== -1 && this._tagStack[pos--].name !== baseName) { }
-					if (pos !== -1 || this._tagStack[0].name === baseName)
-						while(pos < this._tagStack.length - 1)
-							this._tagStack.pop();
-				}
-			}
-			else { //This is not a closing tag
-				if (!lastTag.children)
-					lastTag.children = [];
-				lastTag.children.push(element);
-				if (!this.isEmptyTag(element)) //Don't add tags to the tag stack that can't have children
-					this._tagStack.push(element);
-			}
-		}
-		else { //This is not a container element
-			if (!lastTag.children)
-				lastTag.children = [];
-			lastTag.children.push(element);
-		}
-	}
+}
+
+DefaultHandler.prototype._openTag = function(element){
+	if(!this._options.verbose) delete element.data;
+	
+	this._addDomElement(element);
+	
+	//Don't add tags to the tag stack that can't have children
+	if(!this._isEmptyTag(element.name)) this._tagStack.push(element);
+}
+
+DefaultHandler.prototype.writeText = function(element){
+	if(this._options.ignoreWhitespace && element.data.trim() === "") return;
+	this._addDomElement(element);
+};
+
+DefaultHandler.prototype.writeDirective = DefaultHandler.prototype.writeComment = DefaultHandler.prototype._addDomElement;
+
+DefaultHandler.prototype.writeTag = function(element) {
+	if(element.name.charAt(0) === "/") this._closeTag(element.name.substr(1));
+	else this._openTag(element);
 };
 
 module.exports = DefaultHandler;

tests/00-runtests.js
@@ -31,6 +31,7 @@ var testCount = 0;
 var failedCount = 0;
 var totalTime = 0;
 var name = __filename.split("/").slice(-1)[0];
+var handler;
 for (var i = 1; i < testFiles.length; i++) {
 	if(testFiles[i] === name) continue;
 	testCount++;
@@ -42,14 +43,22 @@ for (var i = 1; i < testFiles.length; i++) {
 	}
 	console.log(testFiles[i]);
 	var start = Date.now();
-	var handler = (test.type === "rss") ?
-		new htmlparser.RssHandler(handlerCallback, test.options.handler)
-		:
-		new htmlparser.DefaultHandler(handlerCallback, test.options.handler)
-		;
+	if(test.type === "rss"){
+		handler = new htmlparser.RssHandler(handlerCallback, test.options.handler);
+	}
+	else if(test.type === "event"){
+		handler = new htmlparser.EventedHandler(test.options.handler);
+	}
+	else{
+		handler = new htmlparser.DefaultHandler(handlerCallback, test.options.handler);
+	}
 	var parser = new htmlparser.Parser(handler, test.options.parser);
 	parser.parseComplete(test.html);
 	var resultComplete = handler.dom;
+	if(test.type === "event"){
+		resultComplete = test.result;
+		test.result = [];
+	}
 	var chunkPos = 0;
 	parser.reset();
 	while (chunkPos < test.html.length) {
@@ -58,6 +67,9 @@ for (var i = 1; i < testFiles.length; i++) {
 	}
 	parser.done();
 	var resultChunk = handler.dom;
+	if(test.type === "event"){
+		resultChunk = test.result;
+	}
 	var testResult =
 		sys.inspect(resultComplete, false, null) === sys.inspect(test.expected, false, null)
 		&&