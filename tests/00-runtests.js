fb55/htmlparser2

Splited writeTag in both handlers to smaller functions + added a test for events

tests/00-runtests.js
@@ -10,6 +10,7 @@ var testCount = 0;
 var failedCount = 0;
 var totalTime = 0;
 var name = __filename.split("/").slice(-1)[0];
+var handler;
 for (var i = 1; i < testFiles.length; i++) {
     if (testFiles[i] === name) continue;
     testCount++;
@@ -20,16 +21,26 @@ for (var i = 1; i < testFiles.length; i++) {
     };
     console.log(testFiles[i]);
     var start = Date.now();
-    var handler =
-        test.type === "rss"
-            ? new htmlparser.RssHandler(handlerCallback, test.options.handler)
-            : new htmlparser.DefaultHandler(
-                  handlerCallback,
-                  test.options.handler
-              );
+    if (test.type === "rss") {
+        handler = new htmlparser.RssHandler(
+            handlerCallback,
+            test.options.handler
+        );
+    } else if (test.type === "event") {
+        handler = new htmlparser.EventedHandler(test.options.handler);
+    } else {
+        handler = new htmlparser.DefaultHandler(
+            handlerCallback,
+            test.options.handler
+        );
+    }
     var parser = new htmlparser.Parser(handler, test.options.parser);
     parser.parseComplete(test.html);
     var resultComplete = handler.dom;
+    if (test.type === "event") {
+        resultComplete = test.result;
+        test.result = [];
+    }
     var chunkPos = 0;
     parser.reset();
     while (chunkPos < test.html.length) {
@@ -38,6 +49,9 @@ for (var i = 1; i < testFiles.length; i++) {
     }
     parser.done();
     var resultChunk = handler.dom;
+    if (test.type === "event") {
+        resultChunk = test.result;
+    }
     var testResult =
         sys.inspect(resultComplete, false, null) ===
             sys.inspect(test.expected, false, null) &&

fasterize/node-htmlparser

tests/00-runtests.js
@@ -39,14 +39,22 @@ for (var i in testFiles) {
 	}
 	console.log(testFiles[i]);
 	var start = Date.now();
-	var handler = (test.type === "rss") ?
-		new htmlparser.RssHandler(handlerCallback, test.options.handler)
-		:
-		new htmlparser.DefaultHandler(handlerCallback, test.options.handler)
-		;
+	if(test.type === "rss"){
+		handler = new htmlparser.RssHandler(handlerCallback, test.options.handler);
+	}
+	else if(test.type === "event"){
+		handler = new htmlparser.EventedHandler(test.options.handler);
+	}
+	else{
+		handler = new htmlparser.DefaultHandler(handlerCallback, test.options.handler);
+	}
 	var parser = new htmlparser.Parser(handler, test.options.parser);
 	parser.parseComplete(test.html);
 	var resultComplete = handler.dom;
+	if(test.type === "event"){
+		resultComplete = test.result;
+		test.result = [];
+	}
 	var chunkPos = 0;
 	parser.reset();
 	while (chunkPos < test.html.length) {
@@ -55,6 +63,9 @@ for (var i in testFiles) {
 	}
 	parser.done();
 	var resultChunk = handler.dom;
+	if(test.type === "event"){
+		resultChunk = test.result;
+	}
 	var testResult =
 		sys.inspect(resultComplete, false, null) === sys.inspect(test.expected, false, null)
 		&&