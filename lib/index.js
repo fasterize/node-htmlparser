fb55/htmlparser2

use the new dom modules, 2.5.0
Attention: The DOM changes slightly.

lib/index.js
@@ -6,9 +6,7 @@ module.exports = {
         return this.Parser;
     },
     get DomHandler() {
-        defineProp(this, "DomHandler", {
-            value: require("domhandler").Handler
-        });
+        defineProp(this, "DomHandler", { value: require("domhandler") });
         return this.DomHandler;
     },
     get FeedHandler() {
@@ -16,9 +14,7 @@ module.exports = {
         return this.FeedHandler;
     },
     get ElementType() {
-        defineProp(this, "ElementType", {
-            value: require("domhandler").ElementType
-        });
+        defineProp(this, "ElementType", { value: require("domelementtype") });
         return this.ElementType;
     },
     get Stream() {
@@ -38,7 +34,7 @@ module.exports = {
         return this.ProxyHandler;
     },
     get DomUtils() {
-        defineProp(this, "DomUtils", { value: require("domhandler").Utils });
+        defineProp(this, "DomUtils", { value: require("domutils") });
         return this.DomUtils;
     },
     // For legacy support

fasterize/node-htmlparser

lib/index.js
@@ -6,7 +6,7 @@ module.exports = {
 		return this.Parser;
 	},
 	get DomHandler(){
-		defineProp(this, "DomHandler", {value:require("domhandler").Handler});
+		defineProp(this, "DomHandler", {value:require("domhandler")});
 		return this.DomHandler;
 	},
 	get FeedHandler(){
@@ -14,7 +14,7 @@ module.exports = {
 		return this.FeedHandler;
 	},
 	get ElementType(){
-		defineProp(this, "ElementType", {value:require("domhandler").ElementType});
+		defineProp(this, "ElementType", {value:require("domelementtype")});
 		return this.ElementType;
 	},
 	get Stream(){
@@ -30,7 +30,7 @@ module.exports = {
 		return this.ProxyHandler;
 	},
 	get DomUtils(){
-		defineProp(this, "DomUtils", {value:require("domhandler").Utils});
+		defineProp(this, "DomUtils", {value:require("domutils")});
 		return this.DomUtils;
 	},
 	// For legacy support