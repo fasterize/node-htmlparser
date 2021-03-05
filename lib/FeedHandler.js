fb55/htmlparser2

use DomUtils.getText in fetch, split getElements

lib/FeedHandler.js
@@ -11,14 +11,15 @@ require("util").inherits(FeedHandler, DomHandler);
 
 FeedHandler.prototype.init = DomHandler;
 
-function getElements(what, where, one, recurse) {
-    if (one) return DomUtils.getElementsByTagName(what, where, recurse, 1)[0];
-    return DomUtils.getElementsByTagName(what, where, recurse);
+function getElements(what, where) {
+    return DomUtils.getElementsByTagName(what, where, false);
+}
+function getOneElement(what, where) {
+    return DomUtils.getElementsByTagName(what, where, false, 1)[0];
 }
 function fetch(what, where, recurse) {
-    var ret = DomUtils.getElementsByTagName(what, where, recurse, 1);
-    return (
-        ret.length > 0 && ret[0].children.length > 0 && ret[0].children[0].data
+    return DomUtils.getText(
+        DomUtils.getElementsByTagName(what, where, !!recurse, 1)
     );
 }
 
@@ -28,7 +29,7 @@ var isValidFeed = function(value) {
 
 FeedHandler.prototype.onend = function() {
     var feed = {},
-        feedRoot = getElements(isValidFeed, this.dom, true),
+        feedRoot = getOneElement(isValidFeed, this.dom),
         tmp,
         childs;
 
@@ -40,7 +41,7 @@ FeedHandler.prototype.onend = function() {
             if ((tmp = fetch("id", childs))) feed.id = tmp;
             if ((tmp = fetch("title", childs))) feed.title = tmp;
             if (
-                (tmp = getElements("link", childs, true)) &&
+                (tmp = getOneElement("link", childs)) &&
                 (tmp = tmp.attribs) &&
                 (tmp = tmp.href)
             )
@@ -58,7 +59,7 @@ FeedHandler.prototype.onend = function() {
                 if ((tmp = fetch("id", item))) entry.id = tmp;
                 if ((tmp = fetch("title", item))) entry.title = tmp;
                 if (
-                    (tmp = getElements("link", item, true)) &&
+                    (tmp = getOneElement("link", item)) &&
                     (tmp = tmp.attribs) &&
                     (tmp = tmp.href)
                 )
@@ -69,7 +70,7 @@ FeedHandler.prototype.onend = function() {
                 return entry;
             });
         } else {
-            childs = getElements("channel", feedRoot.children, true).children;
+            childs = getOneElement("channel", feedRoot.children).children;
 
             feed.type = feedRoot.name.substr(0, 3);
             feed.id = "";

fasterize/node-htmlparser

lib/FeedHandler.js
@@ -9,9 +9,11 @@ function FeedHandler(callback){
 
 inherits(FeedHandler, DefaultHandler);
 
-function getElements(what, where, one, recurse){
-	if(one) return DomUtils.getElementsByTagName(what, where, recurse, 1)[0];
-	return DomUtils.getElementsByTagName(what, where, recurse);
+function getElements(what, where){
+	return DomUtils.getElementsByTagName(what, where, false);
+}
+function getOneElement(what, where){
+	return DomUtils.getElementsByTagName(what, where, false, 1)[0];
 }
 function fetch(what, where, recurse){
 	var ret = getElements(what, where, true, recurse);
@@ -25,7 +27,7 @@ var isValidFeed = function(value) {
 
 FeedHandler.prototype.onend = function() {
 	var feed = {},
-		feedRoot = getElements(isValidFeed, this.dom, true),
+		feedRoot = getOneElement(isValidFeed, this.dom),
 		tmp, childs;
 
 	if (feedRoot) {
@@ -35,7 +37,7 @@ FeedHandler.prototype.onend = function() {
 			feed.type = "atom";
 			if(tmp = fetch("id", childs)) feed.id = tmp;
 			if(tmp = fetch("title", childs)) feed.title = tmp;
-			if((tmp = getElements("link", childs, true)) && (tmp = tmp.attribs) && (tmp = tmp.href)) feed.link = tmp;
+			if((tmp = getOneElement("link", childs)) && (tmp = tmp.attribs) && (tmp = tmp.href)) feed.link = tmp;
 			if(tmp = fetch("subtitle", childs)) feed.description = tmp;
 			if(tmp = fetch("updated", childs)) feed.updated = new Date(tmp);
 			if(tmp = fetch("email", childs, true)) feed.author = tmp;
@@ -47,13 +49,13 @@ FeedHandler.prototype.onend = function() {
 
 				if(tmp = fetch("id", item)) entry.id = tmp;
 				if(tmp = fetch("title", item)) entry.title = tmp;
-				if((tmp = getElements("link", item, true)) && (tmp = tmp.attribs) && (tmp = tmp.href)) entry.link = tmp;
+				if((tmp = getOneElement("link", item)) && (tmp = tmp.attribs) && (tmp = tmp.href)) entry.link = tmp;
 				if(tmp = fetch("summary", item)) entry.description = tmp;
 				if(tmp = fetch("updated", item)) entry.pubDate = new Date(tmp);
 				return entry;
 			});
 		} else{
-			childs = getElements("channel", feedRoot.children, true).children;
+			childs = getOneElement("channel", feedRoot.children).children;
 
 			feed.type = feedRoot.name.substr(0, 3);
 			feed.id = "";