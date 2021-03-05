fb55/htmlparser2

Renamed DefaultHandler to DomHandler
The new name expresses better what it does.

The old name is still available via
`require("htmlparser2").DefaultHandler`, so support for code written
for `htmlparser` didn't break.

lib/index.js
@@ -1,38 +1,36 @@
+var defineProp = Object.defineProperty;
+
 module.exports = {
     get Parser() {
-        Object.defineProperty(this, "Parser", {
-            value: require("./Parser.js")
-        });
+        defineProp(this, "Parser", { value: require("./Parser.js") });
         return this.Parser;
     },
-    get DefaultHandler() {
-        Object.defineProperty(this, "DefaultHandler", {
-            value: require("./DefaultHandler.js")
-        });
-        return this.DefaultHandler;
+    get DomHandler() {
+        defineProp(this, "DomHandler", { value: require("./DomHandler.js") });
+        return this.DomHandler;
     },
     get FeedHandler() {
-        Object.defineProperty(this, "FeedHandler", {
-            value: require("./FeedHandler.js")
-        });
+        defineProp(this, "FeedHandler", { value: require("./FeedHandler.js") });
         return this.FeedHandler;
     },
     get ElementType() {
-        Object.defineProperty(this, "ElementType", {
-            value: require("./ElementType.js")
-        });
+        defineProp(this, "ElementType", { value: require("./ElementType.js") });
         return this.ElementType;
     },
     get Stream() {
-        Object.defineProperty(this, "Stream", {
-            value: require("./Stream.js")
-        });
+        defineProp(this, "Stream", { value: require("./Stream.js") });
         return this.Stream;
     },
     get DomUtils() {
-        Object.defineProperty(this, "DomUtils", {
-            value: require("./DomUtils.js")
-        });
+        defineProp(this, "DomUtils", { value: require("./DomUtils.js") });
         return this.DomUtils;
+    },
+    get DefaultHandler() {
+        defineProp(this, "DefaultHandler", { value: this.DomHandler });
+        return this.DefaultHandler;
+    },
+    get RssHandler() {
+        defineProp(this, "RssHandler", { value: this.FeedHandler });
+        return this.FeedHandler;
     }
 };

fasterize/node-htmlparser

lib/index.js
@@ -1,26 +1,36 @@
+var defineProp = Object.defineProperty;
+
 module.exports = {
 	get Parser(){
-		Object.defineProperty(this, "Parser", {value:require("./Parser.js")});
+		defineProp(this, "Parser", {value:require("./Parser.js")});
 		return this.Parser;
 	},
-	get DefaultHandler(){
-		Object.defineProperty(this, "DefaultHandler", {value:require("./DefaultHandler.js")});
-		return this.DefaultHandler;
+	get DomHandler(){
+		defineProp(this, "DomHandler", {value:require("./DomHandler.js")});
+		return this.DomHandler;
 	},
 	get FeedHandler(){
-		Object.defineProperty(this, "FeedHandler", {value:require("./FeedHandler.js")});
+		defineProp(this, "FeedHandler", {value:require("./FeedHandler.js")});
 		return this.FeedHandler;
 	},
 	get ElementType(){
-		Object.defineProperty(this, "ElementType", {value:require("./ElementType.js")});
+		defineProp(this, "ElementType", {value:require("./ElementType.js")});
 		return this.ElementType;
 	},
 	get Stream(){
-		Object.defineProperty(this, "Stream", {value:require("./Stream.js")});
+		defineProp(this, "Stream", {value:require("./Stream.js")});
 		return this.Stream;
 	},
 	get DomUtils(){
-		Object.defineProperty(this, "DomUtils", {value:require("./DomUtils.js")});
+		defineProp(this, "DomUtils", {value:require("./DomUtils.js")});
 		return this.DomUtils;
+	},
+	get DefaultHandler(){
+		defineProp(this, "DefaultHandler", {value: this.DomHandler});
+		return this.DefaultHandler;
+	},
+	get RssHandler(){
+		defineProp(this, "RssHandler", {value: this.FeedHandler});
+		return this.FeedHandler;
 	}
 }